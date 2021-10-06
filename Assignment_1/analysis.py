ada6fefbcdc9ba5b7d597bdfef328714b493fcfe
Hana Nguyen <nguyennq23@mail.wlu.edu>
2021-10-06 17:41:30 -0400

    Made some progress? got a list of the letters in oreder of frequency

--- Assignment_1/analysis.py
+++ Assignment_1/analysis.py
@@ -4,13 +4,23 @@ Program to crack encrypted.txt
 
 
 def main():
+    encrypted = open("encrypted.txt", "r")
+    decrypted = open("decrypted.txt", "w")
     # Go through the encrypted text and get the number of each letter
+    # and put it in this dictionary
     freqTable = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0,
                  "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0,
                  "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0,
                  "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0,
                  "y": 0, "z": 0}
-    encrypted = open("encrypted.txt", "r")
+    keyList = []
+
+    # http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
+    # A list of all letters put in order of frequency 
+    # (e is most common then t)
+    letterFreqTable = ["e", "t", "a", "o", "i", "n", "s", "r", "h",
+                       "d", "l", "u", "c", "m", "f", "y", "w", "g",
+                       "p", "b", "v", "k", "x", "q", "j", "z"]
     # https://stackoverflow.com/questions/1155617/count-the-number-of-occurrences-of-a-character-in-a-string
     for word in encrypted:
         for key in freqTable:
@@ -19,6 +29,19 @@ def main():
     for key in freqTable:
         print(key + ":", end="")
         print(freqTable[key])
+
+    while freqTable != {}:
+        highestKey = "a"
+        for key in freqTable:
+            if freqTable[key] > freqTable[highestKey]:
+                highestKey = key
+        
+        keyList.append(highestKey)
+        freqTable.pop(highestKey)
+    print(keyList)
+
+
+
 
 
 if __name__ == "__main__":
