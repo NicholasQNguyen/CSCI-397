19ceab0221f9bb4018fe792d47ac67f88a4dd7dc
Hana Nguyen <nguyennq23@mail.wlu.edu>
2021-10-08 12:05:28 -0400

    progress?

--- Assignment_1/cracker.py
+++ Assignment_1/cracker.py
@@ -2,28 +2,39 @@
 Program to crack hashed passwords from shadow.txt using dictionary.txt
 """
 import hashlib
+from hashlib import blake2b
 
 def main():
     dictionary = open("dictionary.txt", "r")
+    passwords = open("password,txt", "w")
     shadow = open("shadow", "r")
 
     shadowHashes = []
+
+    # https://docs.python.org/3/library/hashlib.html
+    m = hashlib.md5()
 
     # Create a list of just the hashes w/o the user#: and \n
     for line in shadow:
         shadowHashes.append(line[6:].strip())
 
-    for hash in shadowHashes:
-        print(hash)
+    # for hash in shadowHashes:
+    #     print(hash)
 
     #Go through the lines of the dictionary
+    for line in dictionary:
+        encodedLine = line.encode()
+        hashedLine = hashlib.sha256(encodedLine).hexdigest()
         #hash the line
-            #go through the lines of shadowHashes
-                #Compare the hash we calculated to each of the shadowHashes
-                    #If we get a match, print out line
+        # print(hashedLine)
+        #go through the lines of shadowHashes
+        for hashes in shadowHashes:
+            # print(hashes)
+            #Compare the hash we calculated to each of the shadowHashes
+            if hashedLine == hashes:
+                #If we get a match, print out line
+                print(line)
 
-    #https://www.geeksforgeeks.org/md5-hash-python/
-    ## print(hashlib.md5(b"Nicholas").digest())
     for i in range(100000):
         #try to hash it with every salt 1-99999
         #append i onto the string we'll hash
