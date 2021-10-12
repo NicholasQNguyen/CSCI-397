2fc3a2e08e95470ef7632fd3d11061254505e60f
nicholasqnguyen8 <nicholasqnguyen8@gmail.com>
2021-10-12 15:30:42 -0400

    it was the newline character all along!

--- Assignment_1/cracker.py
+++ Assignment_1/cracker.py
@@ -17,17 +17,17 @@ def main():
 
     # Create a list of just the hashes w/o the user#: and \n
     for line in shadow:
-        shadowHashes.append(line[6:].strip())
+        shadowHashes.append(line[6:len(line)].strip())
 
     # for hash in shadowHashes:
     #     print(hash)
     #Go through the lines of the dictionary
     for line in dictionary:
-        encodedLine = line.encode()
+        # Hash the line
+        encodedLine = line[:len(line) - 1].encode()
         hashedLine = hashlib.md5(encodedLine).hexdigest()
         # print(hashedLine)
-        # hash the line
-        #go through the lines of shadowHashes
+        #go through the hashes in shadowHashes
         for hashes in shadowHashes:
             # print(hashes)
             #Compare the hash we calculated to each of the shadowHashes
@@ -36,11 +36,37 @@ def main():
                 print(line)
 
     for i in range(100000):
-        #try to hash it with every salt 1-99999
+        #try to hash it with every salt 0-99999
         #append i onto the string we'll hash
         for line in dictionary:
-            encodedLine = line.append(i).encode()
+            encodedLine = line[:len(line) - 1].append(i).encode()
             hashedLine = hashlib.md5(encodedLine).hexdigest()
+            for hashes in shadowHashes:
+                if hashedLine == hashes:
+                    print(line)
+
+   # for hash in shadowHashes:
+    #     print(hash)
+    #Go through the lines of the dictionary
+    for line in dictionary:
+        # Hash the line
+        encodedLine = line[:len(line) - 1].encode()
+        hashedLine = hashlib.sha1(encodedLine).hexdigest()
+        # print(hashedLine)
+        #go through the hashes in shadowHashes
+        for hashes in shadowHashes:
+            # print(hashes)
+            #Compare the hash we calculated to each of the shadowHashes
+            if hashedLine == hashes:
+                #If we get a match, print out line
+                print(line)
+
+    for i in range(100000):
+        #try to hash it with every salt 0-99999
+        #append i onto the string we'll hash
+        for line in dictionary:
+            encodedLine = line[:len(line) - 1].append(i).encode()
+            hashedLine = hashlib.sha1(encodedLine).hexdigest()
             for hashes in shadowHashes:
                 if hashedLine == hashes:
                     print(line)
